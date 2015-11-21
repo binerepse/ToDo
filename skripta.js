f261ac24d686eddc8846af00dc6e0da94624802c
binerepse <bine.repse@gmail.com>
2015-11-21 15:18:05 +0100

    Popravki, spremenjen font h1.

--- skripta.js
+++ skripta.js
@@ -6,10 +6,14 @@ $(document).ready(function(){
 	$("#dodajOpravilo").click(function(){
 		var opravilo = $("#vnosOpravila").val();
 		var datum = "<p>Rok opravila: "+$("#izbiraRoka").val()+"<\p>";
-		var kategorija = "<p> Kategorija: "+$("#izbiraKategorije").val()+"<\p>";
-		var prioriteta = "<p> Prioriteta: "+$("#sel1").val()+"<\p>";
-		var opis = "<p>"+$("#opisOpravila").val()+"<\p>";
-		if(datum ==="<p>Rok opravila: <\p>"){
+		var kategorija = "<p>Kategorija: "+$("#izbiraKategorije").val()+"<\p>";
+		var prioriteta = "<p>Prioriteta: "+$("#sel1").val()+"<\p>";
+		var opis = "<p>Opis: "+$("#opisOpravila").val()+"<\p>";
+		if( datum ==="<p>Rok opravila: <\p>" && opis === "<p>Opis: <\p>"){
+			opis = kategorija + prioriteta;
+		}else if (opis === "<p>Opis: <\p>"){
+			opis = datum + kategorija + prioriteta;
+		}else if (datum ==="<p>Rok opravila: <\p>"){
 			opis = opis + kategorija + prioriteta;
 		}else{
 			opis = opis + datum + kategorija + prioriteta;
@@ -19,7 +23,7 @@ $(document).ready(function(){
 			stevecOpravil = stevecOpravil + 1;
 			seznamOpravil.push(opravilo);
 			
-			$(".add").toggle("slow");
+			$(".add").toggle();
 			$("#novoOpravilo").toggle();	
 			$("#todo").append("<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a data-toggle=\"collapse\" data-parent=\"#todo\" href=\"#collapse"+stevecOpravil+"\">"+opravilo+"<a href=\"#\"><span class=\"glyphicon glyphicon-ok\" id=\"icon\"></span></a></a></h4></div><div id=\"collapse"+stevecOpravil+"\" class=\"panel-collapse collapse in\"><div class=\"panel-body\">"+opis+"</div></div></div>");
 			
@@ -39,7 +43,7 @@ $(document).ready(function(){
 	
 	$("#novoOpravilo").click(function(){
 		$(this).toggle();
-		$(".add").toggle("fast");
+		$(".add").toggle("fast","linear");
 	});
 	
 	
