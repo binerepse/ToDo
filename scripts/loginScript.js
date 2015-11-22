4c99fe29bc9ce10817141897b252214cdc46d893
binerepse <bine.repse@gmail.com>
2015-11-22 17:11:04 +0100

    validiranje emaila dela, login pa ne preusmeri na homepage

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	});
 	
 	var validateEmail = function(email){
-		var regex = /\S+@\S+\.\S+/;
+		var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		
 		return regex.test(email);
 	}
