8e7f64221b6551b80a5d2202bf99a9e76b2572dc
binerepse <bine.repse@gmail.com>
2015-11-22 21:15:21 +0100

    čiščenje in komentiranje kode

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
+	//FUNKCIJA, KI OB KLIKU NA GUMB LOGIN PREVERI EMAIL TER UPORABNIKA OPOMNI, V PRIMERU, KO JE LE TA NAPAČEN
 	$(".btn-default").click(function(){
 		var email = $("#email").val();
 		if (!validateEmail(email)){
-			alert("Vneseni email ni pravilen.")
+			alert("Vneseni email ni pravilen.");
 		}
 	});
 	
+	//FUNKCIJA ZA PREVERJANJE PRAVILNOSTI EMAILA
 	var validateEmail = function(email){
 		var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		
--- scripts/skripta.js
+++ scripts/skripta.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
 	
 	var stevecOpravil = 0;
 	var seznamOpravil = [];
-	//ob pritisku na gumb Dodaj, dodamo opravilo v seznam opravil, ki jih je še potrebno opraviti.
+	//FUNKCIJA, KI DOLOČA KAJ SE ZGODI KO KLIKNEMO NA GUMB DODAJ
 	$("#dodajOpravilo").click(function(){
+	//POBIRANJE VREDNOSTI IZ OBRAZCA
 		var opravilo = $("#vnosOpravila").val();
 		var datum = "<p>Rok opravila: "+$("#izbiraRoka").val()+"<\p>";
 		var kategorija = "<p>Kategorija: "+$("#izbiraKategorije").val()+"<\p>";
 		var prioriteta = "<p>Prioriteta: "+$("#sel1").val()+"<\p>";
 		var opis = "<p>Opis: "+$("#opisOpravila").val()+"<\p>";
+		//SESTAVLJANJE OPISA DODANEGA OPRAVILA
 		if( datum ==="<p>Rok opravila: <\p>" && opis === "<p>Opis: <\p>"){
 			opis = kategorija + prioriteta;
 		}else if (opis === "<p>Opis: <\p>"){
@@ -18,8 +20,8 @@ $(document).ready(function(){
 		}else{
 			opis = opis + datum + kategorija + prioriteta;
 		}
+		//PREVERJANJE, ALI JE OPRAVILO Z ENAKIM IMENOM ŽE DODANO TER ALI UPORABNIK NI VNESEL NIČ
 		if (opravilo.length > 0 && seznamOpravil.indexOf(opravilo) === -1){
-			//$("#todo").append("<li class=\"list-group-item\"><input type=\"checkbox\" class=\"checkbox\">"+opravilo+"</li>");	
 			stevecOpravil = stevecOpravil + 1;
 			seznamOpravil.push(opravilo);
 			$(".add").toggle();
@@ -33,23 +35,24 @@ $(document).ready(function(){
 			alert("Poskusili ste dodati prazno opravilo, ali pa opravilo ki že obstaja.");
 		}
 	});
+	
+	//FUNKCIJA, KI OPRAVILO IZBRIŠE IZ SEZNAMA TODO NA SEZNAM DONE S POMOČJO KLIKA NA KLJUKICO
 	$(document).on("click", "#icon" ,function(){
 		$(this).parent().parent().parent().parent().prependTo("#done");
 		$(this).remove();
 		
 	});
 	
+	//OMEJITEV DATEPICKERJA NA MINIMALNI MOŽEN DATUM: DANES
 	$("#izbiraRoka").datepicker({
 		minDate: "0"
 	});
 	
+	//FUNKCIJA, KI SKRIJE OBRAZEC ZA OPIS OPRAVILA, PO TEM KO UPORABNIK KLIKNE GUMB DODAJ
 	$("#novoOpravilo").click(function(){
 		$(this).toggle();
 		$(".add").toggle("fast","linear");
 	});
-	
-	
-	
-	
+
 });
 
